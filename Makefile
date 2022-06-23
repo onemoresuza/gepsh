c2b63a59c45e327ab5b0f834c0ea614ce9707d3a
Gustavo Coutinho de Souza <dev@onemoresuza.mailer.me>
2022-06-23 10:05:21 -0300

    Rename `__is_in()` to `__get_argtype()`
    The new behavior of the `__is_in()` function ---
    now `__get_argtype()` --- is to print to stdout
    the type of argument of an option that matches any
    of the ones on the given string list. If no
    matches are found, the behavior is the same as
    before, it returns 1.

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ TEST_ENV := test_env
 TEST_ENV_SCRIPT_DIR := $(TEST_ENV)/scripts
 TEST_ENV_HELPER_SCRIPT := $(TEST_ENV_SCRIPT_DIR)/helper.sh
 
-.PHONY: all install uninstall test test-__is_in
+.PHONY: all install uninstall test test-__get_argtype
 
 all:
 	@echo "Available Targets Are as Follows:\n"
@@ -13,7 +13,7 @@ all:
 	@echo "test: run all the tests."
 	@echo "test-__is_in: run the test for the \"__is_in()\" function."
 
-test: test-__is_in
+test: test-__get_argtype
 
-test-__is_in:
-	$(SHELL) $(TEST_ENV_SCRIPT_DIR)/__is_in.sh $(LIB_FILE) $(TEST_ENV_HELPER_SCRIPT)
+test-__get_argtype:
+	$(SHELL) $(TEST_ENV_SCRIPT_DIR)/__get_argtype.sh $(LIB_FILE) $(TEST_ENV_HELPER_SCRIPT)
--- test_env/scripts/__get_argtype.sh
+++ test_env/scripts/__get_argtype.sh
@@ -17,16 +17,16 @@ set -e
 #
 # These next statements are related to the helper.sh script (${2}).
 #
-export TESTNAME="__is_in() test"
+export TESTNAME="__get_argtype() test"
 alias rperr='helper_rperr'
 
 #
 # Global variables used in all tests.
-__IS_IN_TARGET=""
-__IS_IN_LIST=""
-__IS_IN_ARG_TYPE=""
-__IS_IN_REQARG="reqarg"
-__IS_IN_NOARG="noarg"
+__GET_ARGTYPE_TARGET=""
+__GET_ARGTYPE_LIST=""
+__GET_ARGTYPE_ARG_TYPE=""
+__GET_ARGTYPE_REQARG="reqarg"
+__GET_ARGTYPE_NOARG="noarg"
 
 #
 # Short Options
@@ -35,33 +35,34 @@ __IS_IN_NOARG="noarg"
 #
 # The expected results are:
 #
-# 1) The "__IS_IN_ARG_TYPE" is set;
-# 2) The set value of "__IS_IN_ARG_TYPE" is "noarg".
+# 1) The "__GET_ARGTYPE_ARG_TYPE" is set;
+# 2) The set value of "__GET_ARGTYPE_ARG_TYPE" is "noarg".
 #
-__IS_IN_TARGET="a"
-__IS_IN_LIST="a,b,c"
-__is_in_cmd() { __is_in "${__IS_IN_TARGET}" "${__IS_IN_LIST}"; }
-__IS_IN_CMD="$(printf "__is_in \"%s\" \"%s\"" \
-  "${__IS_IN_TARGET}" "${__IS_IN_LIST}")"
-__IS_IN_ARG_TYPE="$(__is_in_cmd)" || {
+__GET_ARGTYPE_TARGET="a"
+__GET_ARGTYPE_LIST="a,b,c"
+__get_argtype_cmd() { __get_argtype "${__GET_ARGTYPE_TARGET}" "${__GET_ARGTYPE_LIST}"; }
+__GET_ARGTYPE_CMD="$(printf "__get_argtype \"%s\" \"%s\"" \
+  "${__GET_ARGTYPE_TARGET}" "${__GET_ARGTYPE_LIST}")"
+__GET_ARGTYPE_ARG_TYPE="$(__get_argtype_cmd)" || {
   rperr "${C_TITLE}Short Option Test (Matching):${C_NORMAL} "
   TESTNAME="__HIDE__" rperr "${C_FAIL}FAILED${C_NORMAL}.\n"
   rperr "\"${C_TARGET}%s${C_NORMAL}\" was supposed to be found among " \
-    "${__IS_IN_TARGET}"
+    "${__GET_ARGTYPE_TARGET}"
   TESTNAME="__HIDE__" rperr "\"${C_TARGET}%s${C_LIST},%s${C_NORMAL}\".\n" \
-    "${__IS_IN_TARGET}" "${__IS_IN_LIST#"${__IS_IN_TARGET},"}"
+    "${__GET_ARGTYPE_TARGET}" "${__GET_ARGTYPE_LIST#"${__GET_ARGTYPE_TARGET},"}"
   rperr "${C_TITLE}Failed Command:${C_NORMAL}\n"
-  TESTNAME="__HIDE__" rperr "\t${C_CMD}$ %s${C_NORMAL}\n" "${__IS_IN_CMD}"
+  TESTNAME="__HIDE__" rperr "\t${C_CMD}$ %s${C_NORMAL}\n" "${__GET_ARGTYPE_CMD}"
   exit 1
 }
 
-[ "${__IS_IN_ARG_TYPE}" != "${__IS_IN_NOARG}" ] && {
+[ "${__GET_ARGTYPE_ARG_TYPE}" != "${__GET_ARGTYPE_NOARG}" ] && {
   rperr "${C_TITLE}Short Option Test (Argument Type):${C_NORMAL} "
   TESTNAME="__HIDE__" rperr "${C_FAIL}FAILED${C_NORMAL}.\n"
   rperr "\"${C_TARGET}%s${C_NORMAL}\" was the expected output to stdout.\n" \
-    "${__IS_IN_NOARG}"
-  rperr "Instead got \"${C_FAIL}%s${C_NORMAL}\".\n" "${__IS_IN_ARG_TYPE}"
+    "${__GET_ARGTYPE_NOARG}"
+  rperr "Instead got \"${C_FAIL}%s${C_NORMAL}\".\n" "${__GET_ARGTYPE_ARG_TYPE}"
   rperr "${C_TITLE}Failed Command:${C_NORMAL}\n"
-  TESTNAME="__HIDE__" rperr "\t${C_CMD}$ %s${C_NORMAL}\n" "${__IS_IN_CMD}"
+  TESTNAME="__HIDE__" rperr "\t${C_CMD}$ %s${C_NORMAL}\n" "${__GET_ARGTYPE_CMD}"
   exit 1
 }
+printf "SUCCESS"

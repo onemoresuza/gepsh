8ff78c53affa4d60d61d445f30f4bcee79488a4b
Gustavo Coutinho de Souza <dev@onemoresuza.mailer.me>
2022-06-26 11:42:02 -0300

    Update test scripts to make use of new global vars
    The test scripts for the `__get_argtype()`
    function now make use of the global variables
    described in the commit below:
    
        > commit 9dec099895fef753e8f6ebbdeb392a66b89a61e6
        > Author: Gustavo Coutinho de Souza <dev@onemoresuza.mailer.me>
        > Date:   Sun Jun 26 11:37:16 2022 -0300

--- test_env/scripts/__get_argtype.sh
+++ test_env/scripts/__get_argtype.sh
@@ -12,13 +12,11 @@ oneTimeSetUp() {
   BO="\033[1m"
   IT="\033[3m"
   NO="\033[m"
-  REQARG="reqarg"
-  NOARG="noarg"
   . ./gepsh
 }
 
 oneTimeTearDown() {
-  unset BO BI NO REQARG NOARG
+  unset BO BI NO
 }
 
 tearDown() {
@@ -63,28 +61,28 @@ testArgTypeRecognition() {
   test_title="Argument type recognition test on argless short option:"
   test_cmd="\$ __get_argtype \"${str}\" \"${str_list}\""
   assertEquals " ${IT}${test_title}${NO}\n\t${BO}${test_cmd}${NO}\n\t" \
-    "${NOARG}" "$(__get_argtype "${str}" "${str_list}")"
+    "${__GEPSH_NOARG}" "$(__get_argtype "${str}" "${str_list}")"
 
   str="a"
   str_list="a:,b,c"
   test_title="Argument type recognition test on short option with argument:"
   test_cmd="\$ __get_argtype \"${str}\" \"${str_list}\""
   assertEquals " ${IT}${test_title}${NO}\n\t${BO}${test_cmd}${NO}\n\t" \
-    "${REQARG}" "$(__get_argtype "${str}" "${str_list}")"
+    "${__GEPSH_REQARG}" "$(__get_argtype "${str}" "${str_list}")"
 
   str="a-long"
   str_list="a-long,b-long,c-long"
   test_title="Argument type recognition test on argless long option:"
   test_cmd="\$ __get_argtype \"${str}\" \"${str_list}\""
   assertEquals " ${IT}${test_title}${NO}\n\t${BO}${test_cmd}${NO}\n\t" \
-    "${NOARG}" "$(__get_argtype "${str}" "${str_list}")"
+    "${__GEPSH_NOARG}" "$(__get_argtype "${str}" "${str_list}")"
 
   str="a-long"
   str_list="a-long:,b-long,c-long"
   test_title="Argument type recognition test on long option with argument:"
   test_cmd="\$ __get_argtype \"${str}\" \"${str_list}\""
   assertEquals " ${IT}${test_title}${NO}\n\t${BO}${test_cmd}${NO}\n\t" \
-    "${REQARG}" "$(__get_argtype "${str}" "${str_list}")"
+    "${__GEPSH_REQARG}" "$(__get_argtype "${str}" "${str_list}")"
 
   return 0
 }
